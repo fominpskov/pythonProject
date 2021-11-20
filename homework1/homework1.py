a463ce749abafc73a5bf01030da33c6686da420e
Антон <fomin.pskov@mail.ru>
2021-11-20 14:59:27 +0300

    Initial commit homework1

---
+++ homework1/homework1.py
@@ -0,0 +1,23 @@
+current_year = 2021
+year_of_birth = 1980
+print(current_year -year_of_birth)
+#
+
+x = 152
+y = 132
+print(x/y)
+print((x/y)*13)
+print(((x/y)*13) **  ( 0.5 ))
+print(int(((x/y)*13) **  ( 0.5 )))
+#
+
+code_1 = "354"
+code_2 = "3"
+code_3 = "132"
+print(str( code_1 + "-" + code_2 + "-" + code_3 ))
+#
+
+a = "Hello Andrey python6."
+b = "I,am 41 years old."
+c = "my secret code is 354-3-132."
+print(str( a + b + c ))
